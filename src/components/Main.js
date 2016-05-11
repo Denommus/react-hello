890ca981dc1bbba1203b7f92bb17a55369aa641e
Yuri Albuquerque <yuri.albuquerque@sumone.com.br>
2016-05-11 16:29:55 -0300

    Alternative implementation

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,17 +2,25 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import RxReact from 'rx-react';
 
 let yeomanImage = require('../images/yeoman.png');
 
-const AppComponent = ({plusOne, minusOne, totalCount}) => {
-    return (
+class AppComponent extends RxReact.Component {
+    getStateStream() {
+        return this.props.totalCount.map(x => {return { totalCount: x };});
+    }
+
+    render() {
+        let totalCount = this.state? this.state.totalCount : 0;
+        return (
             <div>
-              <button onClick={minusOne}>-</button>
+              <button onClick={_ => this.props.minusOne.onNext(null)}>-</button>
               [{totalCount}]
-              <button onClick={plusOne}>+</button>
+              <button onClick={_ => this.props.plusOne.onNext(null)}>+</button>
             </div>
-    );
-};
+        );
+    }
+}
 
 export default AppComponent;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Subject, Observable} from 'rx';
-import createContainer from 'rx-react-container';
 import App from './components/Main.js';
 
 const plusOne$ = new Subject();
@@ -15,5 +14,5 @@ const totalCount$ = Observable
           .startWith(0)
           .scan((acc, x) => acc + x, 0);
 
-const app$ = createContainer(App, {totalCount$}, {plusOne$, minusOne$});
-app$.forEach(renderApp => ReactDOM.render(renderApp(), document.getElementById('app')));
+ReactDOM.render(<App plusOne={plusOne$} minusOne={minusOne$} totalCount={totalCount$} />,
+             document.getElementById("app"));
